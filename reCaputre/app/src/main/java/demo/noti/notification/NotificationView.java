bd6a45595b5d6b1a67923c50c75b0e09f40447e6
Baymax-bibi <erikii2019@outlook.com>
2020-07-08 06:24:57 +0300

    Recapture sec

---
+++ reCaputre/app/src/main/java/demo/noti/notification/NotificationView.java
@@ -0,0 +1,16 @@
+package demo.noti.notification;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationView extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_view);
+
+    }
+}
